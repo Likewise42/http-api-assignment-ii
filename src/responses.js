c5dd219fd1d1f7f2563d4f0ee86a04c46bc5432d
Ryan Muskopf (RIT Student) <rjm9187@ad.rit.edu>
2017-02-22 22:36:45 -0500

    added some stuff from first assignment

---
+++ src/responses.js
@@ -0,0 +1,60 @@
+const fs = require('fs');
+
+const index = fs.readFileSync(`${__dirname}/../client/client.html`);
+const style = fs.readFileSync(`${__dirname}/../client/style.css`);
+
+const convertJSONtoXML = (json) => {
+  let xml = '<response>';
+  xml = `${xml} <msg>${json.msg}</msg>`;
+  xml = `${xml} <id>${json.id}</id>`;
+
+  xml = `${xml} </response>`;
+
+  console.dir(xml);
+  return xml;
+};
+
+const respond = (request, response, status, content, type) => {
+  response.writeHead(status, { 'Content-Type': type });
+
+  console.log(type);
+
+  let cont = content;
+
+  if (type === 'text/xml') {
+    cont = convertJSONtoXML(content, status);
+  } else {
+    cont = JSON.stringify(content);
+  }
+
+  response.write(cont);
+
+  response.end();
+};
+
+const getIndex = (request, response) => {
+  response.writeHead(200, { 'Content-Type': 'text/html' });
+
+  response.write(index);
+
+  response.end();
+};
+module.exports.getIndex = getIndex;
+
+const getStyle = (request, response) => {
+  response.writeHead(200, { 'Content-Type': 'text/css' });
+
+  response.write(style);
+
+  response.end();
+};
+module.exports.getStyle = getStyle;
+
+const notFound = (request, response, params, type) => {
+  const responseJSON = {
+    msg: 'The page was not found',
+    id: 'notFound',
+  };
+  respond(request, response, 404, responseJSON, type);
+};
+module.exports.notFound = notFound;
---
+++ src/server.js
@@ -0,0 +1,39 @@
+const http = require('http');
+const url = require('url');
+const query = require('querystring');
+
+// handelers
+const responseHandler = require('./responses.js');
+
+// port
+const port = process.env.PORT || process.env.NODE_PORT || 3000;
+
+const urlStruct = {
+  '/': responseHandler.getIndex,
+  '/style.css': responseHandler.getStyle,
+  '/getUsers': responseHandler.notFound,
+  '/notReal': responseHandler.notFound,
+  '/addUser': responseHandler.notFound,
+  notFound: responseHandler.notFound,
+};
+
+const onRequest = (request, response) => {
+  // parse the url
+  const parsedUrl = url.parse(request.url);
+  const params = query.parse(parsedUrl.query);
+
+  const acceptedTypes = request.headers.accept.split(',');
+
+  if (urlStruct[parsedUrl.pathname]) {
+    console.log(parsedUrl.pathname);
+    urlStruct[parsedUrl.pathname](request, response, params, acceptedTypes[0]);
+  } else {
+    console.log(parsedUrl.pathname);
+    urlStruct.notFound(request, response, params, acceptedTypes[0]);
+  }
+};
+
+// start HTTP server
+http.createServer(onRequest).listen(port);
+
+console.log(`Listening on 127.0.0.1: ${port}`);
